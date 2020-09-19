881b2497c2bec5d21b678fc8f7dfe536136f3984
ArtyrV <voronaartur3@gmail.com>
2020-09-20 00:41:09 +0300

    Add concurrency and done base working version

--- ServerUSock/ServerUSock.cpp
+++ ServerUSock/ServerUSock.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include<uwebsockets/App.h>
+#include<thread>
+#include<algorithm>
 using namespace std;
 
 // struct for storage user data
@@ -15,18 +17,36 @@ struct UserConnection {
 int main()
 {
     unsigned long latest_user_id = 10;
-    uWS::App().ws<UserConnection>("/*",{
-        .open = [&](auto* ws) {     //[&]- all variables
-            //what to do when user connecting
-            UserConnection* data=(UserConnection* )ws->getUserData();
-            data->user_id = latest_user_id++;
-        },
-        .message = [](auto* ws, string_view message, uWS::OpCode opCode) {
-            // what to  do when receive message
 
-        }
-});
-   
+    vector<thread*> threads(thread::hardware_concurrency());//hardware_concurrency-max can
+    transform(threads.begin(), threads.end(), threads.begin(), 
+        [&latest_user_id](auto* thr) {
+            return new thread(
+                [&latest_user_id] {
+                uWS::App().ws<UserConnection>("/*", {
+                     .open = [&latest_user_id](auto* ws) {     //[&]- all variables
+                            //what to do when user connecting
+                            UserConnection* data = (UserConnection*)ws->getUserData();
+                            data->user_id = latest_user_id++;
+                            cout << "new user connected, id " << latest_user_id << endl;
+                      },
+                     .message = [](auto* ws, string_view message, uWS::OpCode opCode) {
+                            // what to  do when receive message
+                         
+                      }
+
+                    }).listen(9999, 
+                        [](auto* token) {
+                            if (token) {
+                                cout << "Server started and listening on port 9999"<<endl;
+                            }
+                            else{
+                                cout << "Server failed to start("<<endl;
+                            }
+                        }).run();
+            });
+        });
+    for_each(threads.begin(), threads.end(), [](auto* thr) { thr->join();});
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
